248b6061f492ce6cdf5e15a01f2dde09643e48be
Villka <nevermorys@gmail.com>
2021-03-01 08:19:29 +0100

    add editing courses and bug(test)

--- models/course.js
+++ models/course.js
@@ -19,6 +19,24 @@ class Course {
         })
     }
 
+    static async update(course) {
+        const courses = await Course.getAll()
+
+        const index = courses.findIndex(x => x.id === course.id)
+        courses[index] = course
+        return new Promise((resolve, reject) => {
+            fs.writeFile(path.join(__dirname, '..', 'data', 'courses.json'),
+            JSON.stringify(courses),
+            (err) => {
+                if (err) {
+                    reject (err);
+                } else {
+                    resolve()
+                }
+            })
+        })
+    }
+
     async save() {
         const courses = await Course.getAll()
         courses.push(this.toJSON())
@@ -50,6 +68,11 @@ class Course {
                 }
             )
         })
+    }
+
+    static async getById(id) {
+        const courses = await Course.getAll()
+        return courses.find(x => x.id === id)
     }
 }
 
