c3994629b457b2ad1cee6b20a4a03f53882793f5
Villka <nevermorys@gmail.com>
2021-03-08 03:06:46 +0100

    add remove course in the bag

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ const cardRoutes = require('./routes/card');
 const coursesRoutes = require('./routes/courses');
 const addRoutes = require('./routes/add');
 
-
 const app = express();
 
 const hbs = exphbs.create({
@@ -18,8 +17,9 @@ app.engine('hbs', hbs.engine)
 app.set('view engine', 'hbs')
 app.set('views', 'views')
 
-app.use(express.static('public'))
+app.use(express.static(path.join(__dirname, 'public')))
 app.use(express.urlencoded({extended: true}))
+
 app.use('/', homeRoutes)
 app.use('/courses',coursesRoutes)
 app.use('/add', addRoutes)
